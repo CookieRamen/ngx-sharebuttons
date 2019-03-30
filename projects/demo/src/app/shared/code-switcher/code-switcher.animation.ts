9f72afcbd6c68fb723f032f8dd058385d8aff586
CookieRamen <ark@arkjp.net>
2019-03-30 11:33:39 +0900

    Move core to root directory.

--- projects/demo/src/app/shared/code-switcher/code-switcher.animation.ts
+++
@@ -1,9 +0,0 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-
-export const flipperAnimation = trigger('flip', [
-  state('front', style({transform: 'rotateY(0)'})),
-  state('back', style({transform: 'rotateY(180deg)'})),
-  transition('back => front', [
-    animate('600ms ease-in')
-  ])
-]);
